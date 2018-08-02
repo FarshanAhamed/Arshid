00a550de3ad9cb996470c774502d07e60cbc5fe6
Athul Dilip <athuldilip@outlook.com>
2018-08-02 13:16:45 +0530

    User Tables

---
+++ DatabaseMigrator/Program.cs
@@ -0,0 +1,57 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace DatabaseMigrator
+{
+    public class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        static int Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            var connectionString =
+                args.FirstOrDefault()
+                ?? Configuration["SqlConnectionString"];
+
+            EnsureDatabase.For.PostgresqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .PostgresqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+#if DEBUG
+            Console.ReadLine();
+#endif
+            return 0;
+        }
+    }
+}
