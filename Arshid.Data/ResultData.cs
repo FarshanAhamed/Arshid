e28b905585133f8528065ee31cb835df4485794d
Farshan Ahamed <fffarshan8@outlook.com>
2018-08-02 15:16:04 +0530

    ResultData

--- Arshid.Data/ResultData.cs
+++ Arshid.Data/ResultData.cs
@@ -5,6 +5,7 @@ namespace Arshid.Data
     public class ResultData<T>
     {
         public bool Status { get; set; }
-      
+        public string Message { get; set; }
+        public T Data { get; set; }
     }
 }
